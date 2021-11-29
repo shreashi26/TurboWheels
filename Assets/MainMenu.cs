95d61fba10bda8a10e88d862f10a5485d37fde5e
shreashi2631 <shreashi2608@gmail.com>
2021-11-29 14:28:36 +0530

    turbowheels

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGameSwift()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlayGameBumpy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void PlayGameRun()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void PlayGameFlash()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log(" QUIT! ");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/CarControlActive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+
+public class CarControlActive : MonoBehaviour
+{
+
+    public GameObject CarControl;
+    public GameObject AICar1;
+    public GameObject AICar2;
+    public GameObject AICar3;
+
+    void Start()
+    {
+        CarControl.GetComponent<CarController>().enabled = true;
+        AICar1.GetComponent<CarController>().enabled = true;
+        AICar2.GetComponent<CarController>().enabled = true;
+        AICar3.GetComponent<CarController>().enabled = true;
+    }
+}
